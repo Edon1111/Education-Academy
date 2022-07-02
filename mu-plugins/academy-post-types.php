8f82360c6eb2a64deb301c77c182c21ac69364a7
albinsylejmani <albinsylejmani1@gmail.com>
2022-07-02 14:12:21 +0200

    post types

--- mu-plugins/academy-post-types.php
+++ mu-plugins/academy-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function academy_post_types() {
+  // Event Post Type
   register_post_type('event', array(
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +16,23 @@ function academy_post_types() {
       'singular_name' => 'Event'
     ),
     'menu_icon' => 'dashions-calendar'
+  ));
+
+  // Program Post Type
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashions-awards'
   ));
 }
 
--- themes/education-academy-theme/single-program.php
+++ themes/education-academy-theme/single-program.php
@@ -19,23 +19,12 @@ while(have_posts()){
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event') ; ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home </a> <span class="metabox__main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ; ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs </a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
 
         <div class="generic-content"><?php the_content(); ?></div>
-    
-      <?php
-      
-      $relatedPrograms = get_field('related_programs');
-
-      foreach($relatedPrograms as $program){ ?>
-          <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
-      <?php }
-
-      ?>
-    
-      </div>
+    </div>
 
     
  <?php }
