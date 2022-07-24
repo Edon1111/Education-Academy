e61b03957ae1d5b30454c8decae1b6bc89f5e513
Edon1111 <edonterstena@gmail.com>
2022-07-24 12:36:19 +0200

    Update archive-campus.php

--- themes/education-academy-theme/archive-campus.php
+++ themes/education-academy-theme/archive-campus.php
@@ -1,32 +1,25 @@
 <?php
+  
+  get_header();
 
-get_header();
-pageBanner(array(
-  'title' => 'Our Campuses',
-  'subtitle' => 'We have several conveniently located campuses.'
-));
- ?>
-
-<div class="container container--narrow page-section">
-
-<div class="acf-map">
-
-<?php
   while(have_posts()) {
     the_post();
-    $mapLocation = get_field('map_location');
-   ?>
-    <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
-      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-      <?php echo $mapLocation['address']; ?>
+    pageBanner();
+     ?>
+
+    <div class="container container--narrow page-section">
+          <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+      </div>
+
+      <div class="generic-content"><?php the_content(); ?></div>
+
     </div>
-  <?php } ?>
-</div>
+    
 
+    
+  <?php }
 
-
-</div>
-
-<?php get_footer();
+  get_footer();
 
 ?>
